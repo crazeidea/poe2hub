bd70ed3082c0daaa59220391ada89703855d29e9
crazeidea <ganghun@lepisode.team>
2025-05-07 23:25:46 +0900

    chore: updated readme, added .vercelignore, use env for databaseurl

--- apps/app/src/server/trpc/prisma.ts
+++ apps/app/src/server/trpc/prisma.ts
@@ -2,6 +2,5 @@ import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 export const prisma = new PrismaClient({
-  datasourceUrl:
-    'prisma+postgres://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiYjIzOGUxMGMtMDRhNy00ODMxLThiNGQtYWFmNDY2ZmI1NDMzIiwidGVuYW50X2lkIjoiYjZlNjk3YTM4YzY5MTc5NTE0YjZkMDBjNmVlMGI3YzM5YzRmYzI0ZWQxM2M4NzQyYjEwYWE4MWY5MTg0MGM5YSIsImludGVybmFsX3NlY3JldCI6IjNkMGQzNjUwLTNhMGItNDljNS04YjdlLWY3YWFkOGI1MGUzZCJ9.MfN--5PsucFKds7WoJCfU-LpiOzH1bxT-HZYdlIfpnU',
+  datasourceUrl: process.env['DATABASE_URL'],
 }).$extends(withAccelerate());
